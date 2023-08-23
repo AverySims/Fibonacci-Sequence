b0b3cc1622304bd51bf552f96488525de23bd44e
AverySims <avery.j.sims@gmail.com>
2023-08-23 15:18:33 -0500

    Condensed length validation to one 'if' statement

--- Program.cs
+++ Program.cs
@@ -9,27 +9,26 @@
 		static void Main(string[] args)
 		{
 			bool loopMain = true;
-			
+
 			while (loopMain)
 			{
 				// selecting how many numbers in the sequence to generate
 				SelectSequenceLength();
 				// saving generated values to array for later use
 				fibonacciSequence = GenerateFibonacciSequence(sequenceLength);
-				
+
 				SimpleConsoleFunctions.PrintBlank();
 				Console.WriteLine("Sequence start:");
 				for (int i = 0; i < sequenceLength; i++)
 				{
-					Console.WriteLine($"{i+1}. {fibonacciSequence[i]}");
+					Console.WriteLine($"{i + 1}. {fibonacciSequence[i]}");
 				}
+
 				Console.WriteLine("Sequence end");
 				SimpleConsoleFunctions.PrintBlank();
-				
+
 				SimpleConsoleFunctions.SelectEndingAction(out loopMain);
-				
 			}
-			
 		}
 
 		private static void SelectSequenceLength()
@@ -42,22 +41,17 @@
 				// max is 47 steps, ulong begins to break after this
 				if (SimpleConsoleFunctions.ParseIntEC(out sequenceLength))
 				{
-					// check if length is TOO high
-					if (sequenceLength > 47)
-					{
-						Console.WriteLine("Length too high, please enter a lower length (max 47)");
-					}
-					// check if length is TOO low
-					else if (sequenceLength < 1)
-					{
-						// invalid number entered, return to start of loop for new number
-						Console.WriteLine("Please enter a value greater than 0");
-					}
-					else
+					// check if length is TOO low or TOO high
+					if (sequenceLength > 0 && sequenceLength < 48)
 					{
 						// valid number entered, continue the program
 						loopEnable = false;
 					}
+					else
+					{
+						Console.WriteLine("Please choose a valid sequence length (min: 1, max: 47)");
+					}
+                    
 				}
 				else
 				{
